d319da89d039e2beaac894b9eaa120fdd0c3f8c0
Ayyoub BOUMYA <aybbou@gmail.com>
2013-11-04 22:59:39 +0000

    Some changes.

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
+
 require_once 'SelectQuery.php';
 
 use Ayb\Kwiry\SelectQuery;
 
 $query = new SelectQuery();
 
-$query->addField('name')
-        ->addField('age');
-
-$query->addFrom('employee');
-
-$query->addWhere('name = "Ayyoub"')
-        ->addWhere('age < 20');
-
-$query->addOrderBy('name');
+$query->addField('name')->addOrderBy('name')->addWhere('name = "Ayyoub"')
+        ->addField('age')->addWhere('age > 20')
+        ->addFrom('employees');
 
 echo $query->getSQLQuery();
