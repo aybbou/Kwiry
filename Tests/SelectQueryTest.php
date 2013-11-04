b4b524fdd541e6844896029a5792f8e44bf709d8
Ayyoub BOUMYA <aybbou@gmail.com>
2013-11-04 23:53:36 +0000

    Changed the SelectQueryTest file.

--- Tests/SelectQueryTest.php
+++ Tests/SelectQueryTest.php
@@ -13,63 +13,60 @@ use Ayb\Kwiry\SelectQuery;
  */
 class SelectQueryTest extends \PHPUnit_Framework_TestCase {
 
-    private $query;
-
-    public function setUp() {
-        $this->query = new SelectQuery();
-    }
-
-    public function tearDown() {
-        unset($this->query);
-    }
-
     public function testGetFieldsPart() {
-        $this->query->addField('f1');
-        $actual = $this->query->getFieldsPart();
+        $query = new SelectQuery();
+        $query->addField('f1');
+        $actual = $query->getFieldsPart();
         $expected = 'SELECT f1';
         $this->assertEquals($expected, $actual);
     }
 
     public function testGetFromPart() {
-        $this->query->addFrom('t1');
-        $actual = $this->query->getFromPart();
+        $query = new SelectQuery();
+        $query->addFrom('t1');
+        $actual = $query->getFromPart();
         $expected = ' FROM t1';
         $this->assertEquals($expected, $actual);
     }
 
     public function testGetWherePart() {
-        $this->query->addWhere('f1 = "value"');
-        $actual = $this->query->getWherePart();
+        $query = new SelectQuery();
+        $query->addWhere('f1 = "value"');
+        $actual = $query->getWherePart();
         $expected = ' WHERE f1 = "value"';
         $this->assertEquals($expected, $actual);
     }
 
     public function testGetGroupByPart() {
-        $this->query->addGroupBy('f1');
-        $actual = $this->query->getGroupByPart();
+        $query = new SelectQuery();
+        $query->addGroupBy('f1');
+        $actual = $query->getGroupByPart();
         $expected = ' GROUP BY f1';
         $this->assertEquals($expected, $actual);
     }
 
     public function testGetOrderByPart() {
-        $this->query->addOrderBy('f1');
-        $actual = $this->query->getOrderByPart();
+        $query = new SelectQuery();
+        $query->addOrderBy('f1');
+        $actual = $query->getOrderByPart();
         $expected = ' ORDER BY f1';
         $this->assertEquals($expected, $actual);
     }
     
     public function testGetSQLQuery(){
-        $this->query->addField('name')->addFrom('employee')->addField('age')
+        $query = new SelectQuery();
+        $query->addField('name')->addFrom('employee')->addField('age')
                 ->addWhere('age > 20')->addWhere('name = "Ayyoub"')->addOrderBy('name');
-        $actual = $this->query->getSQLQuery();
+        $actual = $query->getSQLQuery();
         $expected = 'SELECT name,age FROM employee WHERE age > 20 AND name = "Ayyoub" ORDER BY name';
         $this->assertEquals($expected, $actual);
-        $this->query = new SelectQuery();
-        $this->query->addFrom('table')->addField('f1')->addField('f2')
+        
+        $query2 = new SelectQuery();
+        $query2->addFrom('table')->addField('f1')->addField('f2')
                 ->addWhere('f1 < value');
-        $actual = $this->query->getSQLQuery();
-        $expected = 'SELECT f1,f2 FROM table WHERE f1 < value';
-        $this->assertEquals($expected, $actual);
+        $actual2 = $query2->getSQLQuery();
+        $expected2 = 'SELECT f1,f2 FROM table WHERE f1 < value';
+        $this->assertEquals($expected2, $actual2);
     }
 
 }
