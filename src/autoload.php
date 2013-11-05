57fa6eea943017923cca68cb7784152cde1f9f52
Ayyoub BOUMYA <aybbou@gmail.com>
2013-11-05 23:58:48 +0000

    changed the autoload file.

--- src/autoload.php
+++ src/autoload.php
@@ -2,9 +2,9 @@
 
 function load($class) {
     $file = __DIR__ . '\\' . $class . '.php';
-    if (file_exists($file)) {
+    //if (file_exists($file)) {
         require_once $file;
-    }
+    //}
 }
 
 spl_autoload_register('load');
