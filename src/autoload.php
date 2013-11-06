bd443558599f9ed9753c4be195f12ee0d01aed48
Ayyoub BOUMYA <aybbou@gmail.com>
2013-11-06 00:02:52 +0000

    Updated the autoload file

--- src/autoload.php
+++ src/autoload.php
@@ -1,10 +1,10 @@
 <?php
 
 function load($class) {
-    $file = __DIR__ . '\\' . $class . '.php';
-    //if (file_exists($file)) {
+    $file = __DIR__ . '/' . $class . '.php';
+    if (file_exists($file)) {
         require_once $file;
-    //}
+    }
 }
 
 spl_autoload_register('load');
